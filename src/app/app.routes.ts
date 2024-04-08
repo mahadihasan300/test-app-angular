dafbe7bd3cc8e0f7c5409e3490c6765fe0fea3c9
Mahadi Hasan <md.mahadihasan300@gmail.com>
2024-04-08 21:01:03 +0600

    tailwind removed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { ContentComponent } from './component/page/content/content.component';
 import { UserRequestComponent } from './component/user-request/user-request.component';
 import { NavbarComponent } from './component/navbar/nav/navbar.component';
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
     {path: '', component: ContentComponent},
-    {path: 'userRequest', component: UserRequestComponent}
+    {path: 'userRequest', component: UserRequestComponent},
+    {path: 'contact', component: ContactComponent}
 ];

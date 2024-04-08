3f716174c36c4a61e6746faf30e41725a2b82569
Mahadi Hasan <md.mahadihasan300@gmail.com>
2024-04-09 01:44:01 +0600

    about section added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,11 @@ import { UserRequestComponent } from './component/user-request/user-request.comp
 import { NavbarComponent } from './component/navbar/nav/navbar.component';
 import { ContactComponent } from './contact/contact.component';
 import { NotFoundPageComponent } from './component/not-found-page/not-found-page.component';
+import { CardsComponent } from './component/cards/cards.component';
 
 export const routes: Routes = [
     {path: 'home','title' : 'Home', component: ContentComponent},
+    {path: 'about','title' : 'Home', component: CardsComponent},
     {path: 'userRequest','title' : 'Service', component: UserRequestComponent},
     {path: 'contact','title' : 'Contact', component: ContactComponent},
     {path: '', redirectTo: '/home', pathMatch: 'full'},

7aacd806ec95a11b0c2c5fe026791a84d8e7f9a0
Mahadi Hasan <md.mahadihasan300@gmail.com>
2024-04-09 01:25:58 +0600

    page not found added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { ContentComponent } from './component/page/content/content.component';
 import { UserRequestComponent } from './component/user-request/user-request.component';
 import { NavbarComponent } from './component/navbar/nav/navbar.component';
 import { ContactComponent } from './contact/contact.component';
+import { NotFoundPageComponent } from './component/not-found-page/not-found-page.component';
 
 export const routes: Routes = [
     {path: '','title' : 'Home', component: ContentComponent},
     {path: 'userRequest','title' : 'Service', component: UserRequestComponent},
-    {path: 'contact','title' : 'Contact', component: ContactComponent}
+    {path: 'contact','title' : 'Contact', component: ContactComponent},
+    {path: '**','title' : 'Page Not Found', component: NotFoundPageComponent}
 ];
